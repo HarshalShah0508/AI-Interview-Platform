b026dd337c175986e56e005cb01b8610505b72d1
Harshal Shah <harshalshah05@Harshals-MacBook-Air.local>
2026-07-23 13:27:12 +0530

    Update CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,7 +17,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://ai-interview-platform-henna-xi.vercel.app",
+        "https://hotseatai.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
